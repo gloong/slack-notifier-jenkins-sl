6b34f7c59a7682e77fde15873259500e435b17da
Dennis W. Otugo <otugodennis@gmail.com>
2018-10-21 16:11:12 +0100

    Update SlackFormatter.groovy

--- src/org/gradiant/jenkins/slack/SlackFormatter.groovy
+++ src/org/gradiant/jenkins/slack/SlackFormatter.groovy
@@ -9,9 +9,9 @@ String format(String title = '', String message = '', String testSummary = '') {
   def buildNumber = helper.getBuildNumber()
   def url = helper.getAbsoluteUrl()
 
-  def result = "${project} » ${branch} - #${buildNumber} ${title.trim()} (<${url}|Open>)"
-  if(message) result = result + "\nChanges:\n\t ${message.trim()}"
-  if(testSummary) result = result + "\n ${testSummary}"
+  def result = "Application:\n<b>${project}</b>\n<b>Branch:</b>\n${branch} - #${buildNumber} ${title.trim()} (<${url}|Open>)"
+  if(message) result = result + "\n<b>Commit:</b>\n\t ${message.trim()}"
+  if(testSummary) result = result + "\n<b>${testSummary}</b>"
 
   return result
 }
